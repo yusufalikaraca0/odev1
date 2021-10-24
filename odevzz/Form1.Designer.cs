bd042ab5e6aed9b62138ed350c3c415f80a5e07c
mttalYangin <patsoluekmek3@gmail.com>
2021-10-24 19:49:25 +0300

    First Commit

---
+++ odevzz/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace odevzz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.daireSonuc = new System.Windows.Forms.GroupBox();
+            this.dikdortgenSonuc = new System.Windows.Forms.GroupBox();
+            this.kareSonuc = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.daireSonuc.SuspendLayout();
+            this.dikdortgenSonuc.SuspendLayout();
+            this.kareSonuc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 74);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dikdörtgen";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kare";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 110);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Daire";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 217);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Location = new System.Drawing.Point(403, 24);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(232, 205);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hesaplama";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(26, 73);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(53, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Çevre";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(26, 40);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(46, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Alan";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.daireSonuc);
+            this.groupBox6.Controls.Add(this.dikdortgenSonuc);
+            this.groupBox6.Controls.Add(this.kareSonuc);
+            this.groupBox6.Location = new System.Drawing.Point(19, 247);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(866, 299);
+            this.groupBox6.TabIndex = 10;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Sonuçlar";
+            // 
+            // daireSonuc
+            // 
+            this.daireSonuc.Controls.Add(this.label6);
+            this.daireSonuc.Controls.Add(this.textBox5);
+            this.daireSonuc.Controls.Add(this.label5);
+            this.daireSonuc.Location = new System.Drawing.Point(605, 31);
+            this.daireSonuc.Name = "daireSonuc";
+            this.daireSonuc.Size = new System.Drawing.Size(232, 242);
+            this.daireSonuc.TabIndex = 9;
+            this.daireSonuc.TabStop = false;
+            this.daireSonuc.Text = "Daire";
+            this.daireSonuc.Visible = false;
+            // 
+            // dikdortgenSonuc
+            // 
+            this.dikdortgenSonuc.Controls.Add(this.textBox3);
+            this.dikdortgenSonuc.Controls.Add(this.label3);
+            this.dikdortgenSonuc.Controls.Add(this.textBox2);
+            this.dikdortgenSonuc.Controls.Add(this.label4);
+            this.dikdortgenSonuc.Location = new System.Drawing.Point(336, 31);
+            this.dikdortgenSonuc.Name = "dikdortgenSonuc";
+            this.dikdortgenSonuc.Size = new System.Drawing.Size(232, 242);
+            this.dikdortgenSonuc.TabIndex = 7;
+            this.dikdortgenSonuc.TabStop = false;
+            this.dikdortgenSonuc.Text = "Dikdörtgen";
+            this.dikdortgenSonuc.Visible = false;
+            // 
+            // kareSonuc
+            // 
+            this.kareSonuc.Controls.Add(this.label2);
+            this.kareSonuc.Controls.Add(this.label1);
+            this.kareSonuc.Controls.Add(this.textBox1);
+            this.kareSonuc.Location = new System.Drawing.Point(17, 31);
+            this.kareSonuc.Name = "kareSonuc";
+            this.kareSonuc.Size = new System.Drawing.Size(276, 242);
+            this.kareSonuc.TabIndex = 8;
+            this.kareSonuc.TabStop = false;
+            this.kareSonuc.Text = "Kare";
+            this.kareSonuc.Visible = false;
+            this.kareSonuc.Enter += new System.EventHandler(this.kareSonuc_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Kenar";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sonuç : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Sonuç : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 31);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 28);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Uzun kenar";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox2_PreviewKeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 65);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 28);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Kısa kenar";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox3_PreviewKeyDown);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(47, 36);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(112, 28);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "Yarıçap";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBox5.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox5_PreviewKeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sonuç : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Sonuç : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 549);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.daireSonuc.ResumeLayout(false);
+            this.daireSonuc.PerformLayout();
+            this.dikdortgenSonuc.ResumeLayout(false);
+            this.dikdortgenSonuc.PerformLayout();
+            this.kareSonuc.ResumeLayout(false);
+            this.kareSonuc.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox daireSonuc;
+        private System.Windows.Forms.GroupBox dikdortgenSonuc;
+        private System.Windows.Forms.GroupBox kareSonuc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ odevzz/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Text;
+namespace odevzz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            string mylocation = Environment.CurrentDirectory;
+            mylocation = mylocation.Substring(0, mylocation.Length - 9);
+            pfc.AddFontFile(mylocation + "cairo.ttf");
+            foreach (Control c in this.Controls)
+            {
+                c.Font = new Font(pfc.Families[0], c.Font.Size + 2, FontStyle.Regular);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        bool firstchecked = true;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            dikdortgenSonuc.Visible = radioButton1.Checked;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            kareSonuc.Visible = radioButton2.Checked;
+        }
+        string seciliislem = "";
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            daireSonuc.Visible = radioButton3.Checked;
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (textBox1.Text == "Kenar")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked)
+                seciliislem = "Alan";
+            foreach (RadioButton c in groupBox1.Controls)
+            {
+                if (c.Checked)
+                {
+                  
+                    if (c.Text == "Kare")
+                        textBox1_TextChanged(sender, e);
+                    else if (c.Text == "Dikdörtgen")
+                        textBox3_TextChanged(sender, e);
+                    else if (c.Text == "Daire")
+                        textBox5_TextChanged(sender, e);
+                }
+            }
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+                seciliislem = "Çevre";
+            foreach (RadioButton c in groupBox1.Controls)
+            {
+                if (c.Checked)
+                {
+                    if (c.Text == "Kare")
+                        textBox1_TextChanged(sender, e);
+                    else if (c.Text == "Dikdörtgen")
+                        textBox2_TextChanged(sender,e);
+                    else if (c.Text == "Daire")
+                        textBox5_TextChanged(sender, e);
+                }
+
+            }
+
+        }
+
+        void hesapla(Label label, TextBox textbox)
+        {
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (seciliislem != "")
+                {
+                    if (seciliislem == "Alan")
+                    {
+                        float sayi = float.Parse(textBox1.Text);
+                        if (float.IsNaN(sayi))
+                            label2.Text = "NaN";
+                        else
+                        {
+                            label2.Text = (sayi * sayi).ToString();
+                        }
+                    }
+                    else if(seciliislem == "Çevre")
+                    {
+                        float sayi = float.Parse(textBox1.Text);
+                        if (float.IsNaN(sayi))
+                            label2.Text = "NaN";
+                        else
+                        {
+                            label2.Text = (sayi * 4).ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("İşlem Seçiniz");
+                }
+            }
+            catch (Exception)
+            {
+                label2.Text = "NaN";
+
+            }
+
+        }
+
+        private void kareSonuc_Enter(object sender, EventArgs e)
+        {
+
+        }
+        
+        void dikdortgen(TextBox textbox,Label label,TextBox textbox2)
+        {
+            try
+            {
+                if (seciliislem != "")
+                {
+                    if (seciliislem == "Alan")
+                    {
+                        float sayi1 = float.Parse(textbox.Text);
+                        float sayi2 = float.Parse(textbox2.Text);
+                        if (float.IsNaN(sayi1))
+                            label.Text = "NaN";
+                        else
+                        {
+                            label.Text = (sayi1* sayi2).ToString();
+                        }
+                    }
+                    else if(seciliislem == "Çevre")
+                    {
+                        float sayi1 = float.Parse(textbox.Text);
+                        float sayi2 = float.Parse(textbox2.Text);
+                        if (float.IsNaN(sayi1))
+                            label.Text = "NaN";
+                        else
+                        {
+                            label.Text = (sayi1*2 + sayi2*2).ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("İşlem Seçiniz");
+                }
+            }
+            catch (Exception)
+            {
+                label.Text = "NaN";
+
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            dikdortgen(textBox2, label4, textBox3);
+
+        }
+
+        private void textBox3_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (textBox3.Text == "Kısa kenar")
+            {
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (textBox2.Text == "Uzun kenar")
+            {
+                textBox2.Text = "";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            dikdortgen(textBox2, label4, textBox3);
+        }
+
+        private void textBox5_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (textBox5.Text == "Yarıçap")
+                textBox5.Text = ""; 
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (seciliislem != "")
+                {
+                    if (seciliislem == "Alan")
+                    {
+                        double sayi1 = double.Parse(textBox5.Text);
+                        
+                        if (double.IsNaN(sayi1))
+                            label6.Text = "NaN";
+                        else
+                        {
+                            label6.Text = Math.Round(Math.PI * (sayi1 * sayi1),2).ToString();
+                        }
+                    }
+                     if (seciliislem == "Çevre")
+                    {
+                        double sayi1 = float.Parse(textBox5.Text);
+                        
+                        if (double.IsNaN(sayi1))
+                            label6.Text = "NaN";
+                        else
+                        {
+                             label6.Text = Math.Round(Math.PI * (sayi1 + sayi1),2).ToString();
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("İşlem Seçiniz");
+                }
+            }
+            catch (Exception)
+            {
+                label6.Text = "NaN";
+
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
